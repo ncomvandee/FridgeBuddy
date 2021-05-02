90b72942a21e8400b3d11590d183d113efa3d7d6
Sam Borhan <samborhan1566@gmail.com>
2021-05-02 15:29:38 -0700

    Add more methods to RecipeModel.ts

--- Models/RecipeModel.ts
+++ Models/RecipeModel.ts
@@ -2,6 +2,7 @@
 import Mongoose = require("mongoose");
 import {DataAccess} from './../DataAccess';
 import {IRecipeModel} from '../Interfaces/IRecipeModel';
+import { IReviewModel } from "../Interfaces/IReviewModel";
 
 let mongooseConnection = DataAccess.mongooseConnection;
 let mongooseObj = DataAccess.mongooseInstance;
@@ -9,6 +10,7 @@ let mongooseObj = DataAccess.mongooseInstance;
 class RecipeModel {
     public schema:any;
     public model:any;
+    public reviewModel:any;
 
     public constructor() {
         this.createSchema();
@@ -32,6 +34,9 @@ class RecipeModel {
 
     public createModel(): void {
         this.model = mongooseConnection.model<IRecipeModel>("recipes", this.schema);
+        this.reviewModel = mongooseConnection.model<IReviewModel>("reviews", this.schema);
+
+        
     }
 
     public retrieveAllRecipes(response:any): any {
@@ -55,6 +60,80 @@ class RecipeModel {
                 else {
                     console.log('Found!' );
                     response.json('{recipeName:' + innerRecipe.recipeName + '}');
+                }
+            }
+        });
+    };
+
+    public addReview(response:any, filter:Object, ReviewId:String){
+        
+        var query = this.model.findeOne({filter});
+        let rate:number = 0;
+        query.exec(function (err, innerRecipe) {
+            if (err) {
+                console.log('error retrieving recipe');
+            }
+            else {
+                if (innerRecipe == null) {
+                    response.status(404);
+                    response.json('{recipeID: Null}');
+                }
+                else {
+                    console.log('Found!' );
+                    for(let i=0; i<innerRecipe.reviewList.length; i++)
+                    {
+                       rate += innerRecipe.reviewList[i].ra
+                    }
+                    response.json('{reviewList:' + innerRecipe.reviewList + '}');
+                }
+            }
+        });
+    };
+
+
+    public removeReview(response:any, filter:Object, ReviewId:String){
+        
+        var query = this.model.findeOne({filter});
+        query.exec(function (err, innerRecipe) {
+            if (err) {
+                console.log('error retrieving recipe');
+            }
+            else {
+                if (innerRecipe == null) {
+                    response.status(404);
+                    response.json('{recipeID: Null}');
+                }
+                else {
+                    console.log('Found!' );
+                    query.reviewList.filter(item => item.id !== ReviewId);
+                    response.json('{reviewList:' + innerRecipe.reviewList + '}');
+                }
+            }
+        });
+    };
+
+    public refreshRating(response:any, filter:Object){
+        
+        var query = this.model.findeOne({filter});
+        let avRate:number = 0;
+        query.exec(function (err, innerRecipe) {
+            if (err) {
+                console.log('error retrieving recipe');
+            }
+            else {
+                if (innerRecipe == null) {
+                    response.status(404);
+                    response.json('{recipeID: Null}');
+                }
+                else {
+                    console.log('Found!' );
+                    for( let i=0; i<innerRecipe.reviewList.length ; i++)
+                    {
+                        var query = this.reviewModel.findeOne.where('reviewID',innerRecipe.reviewList[i]);
+                        avRate += query.rate;
+                    }
+                    innerRecipe.avgRate = avRate / innerRecipe.reviewList.length; 
+                    response.json('{AvgRate:' + innerRecipe.avgRate + '}');
                 }
             }
         });
