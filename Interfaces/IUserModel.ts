16186bfec7cb40387efb7eff146bb93e162adddf
Sam Borhan <samborhan1566@gmail.com>
2021-05-02 10:27:16 -0700

    Add to UserModel and UserINrweface

---
+++ Interfaces/IUserModel.ts
@@ -0,0 +1,14 @@
+import Mongoose = require("mongoose");
+
+
+interface IUsereModel extends Mongoose.Document {
+    userID: String;
+    password: String;
+    email: String;
+    firstName: String;
+    lastName: String;
+    isPremium: boolean;
+    favoritList:Array<String>;
+    recentlyView:Array<String>;
+}
+export {IUsereModel};
