82189b481ccd5c69636827e54e6db5d47267b962
Ken Comvandee <ncomvandee@seattleu.edu>
2021-05-02 02:01:20 -0700

    Initialize DataAccess.ts

---
+++ DataAccess.ts
@@ -0,0 +1,27 @@
+import Mongoose = require("mongoose");
+
+class DataAccess {
+    static mongooseInstance: any;
+    static mongooseConnection: Mongoose.Connection;
+    //static DB_CONNECTION_STRING:string = 'mongodb://dbAdmin:test@localhost:27017/toDoSample?authSource=admin';
+    static DB_CONNECTION_STRING:string = 'mongodb+srv://FridgeBuddy:ilovesaasclass@fridgebuddy.fych5.mongodb.net/CPSC5240?retryWrites=true&w=majority'
+
+    constructor () {
+        DataAccess.connect();
+    }
+    
+    static connect (): Mongoose.Connection {
+        if(this.mongooseInstance) return this.mongooseInstance;
+        
+        this.mongooseConnection  = Mongoose.connection;
+        this.mongooseConnection.on("open", () => {
+            console.log("Connected to mongodb.");
+        });
+        
+        this.mongooseInstance = Mongoose.connect(this.DB_CONNECTION_STRING);
+        return this.mongooseInstance;
+    }
+    
+}
+DataAccess.connect();
+export {DataAccess};
