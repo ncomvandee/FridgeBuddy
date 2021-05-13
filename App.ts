182f18b77ad3199407537b1a0feb80d36eaef3fa
Sam Borhan <samborhan1566@gmail.com>
2021-05-13 11:05:25 -0700

    Add recipe to user's favorite list in App.ts

--- App.ts
+++ App.ts
@@ -92,6 +92,15 @@ class App {
             });
         });
 
+          //  Update user's favorit list by adding a new Recipe
+          router.put('/recipe/addTo/:userId/:recipeId', (req, res) => { 
+            console.log(req.body);
+            var userId = req.params.userId;
+            var recipeId = req.params.userId;
+            this.users.addToFavoriteList(res, userId, recipeId );
+
+        });
+
         this.expressApp.use('/', router);
         
         this.expressApp.use('/app/json/', express.static(__dirname+'/app/json'));
