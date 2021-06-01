c71fce06a7232afc8fa11eb6e65f7c568d3e6905
Ruifeng Wang <rwang@seattleu.edu>
2021-06-01 13:18:50 -0700

    Changed mongoDB password

--- DataAccess.ts
+++ DataAccess.ts
@@ -4,7 +4,7 @@ class DataAccess {
     static mongooseInstance: any;
     static mongooseConnection: Mongoose.Connection;
     //static DB_CONNECTION_STRING:string = 'mongodb://dbAdmin:test@localhost:27017/fridge?authSource=admin';
-    static DB_CONNECTION_STRING:string = 'mongodb+srv://FridgeBuddy:ilovesaasclass@fridgebuddy.fych5.mongodb.net/fridge?retryWrites=true&w=majority'
+    static DB_CONNECTION_STRING:string = 'mongodb+srv://FridgeBuddy:fridge@fridgebuddy.fych5.mongodb.net/fridge?retryWrites=true&w=majority'
 
     constructor () {
         DataAccess.connect();
